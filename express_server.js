2fbe2818b872e984271f78a7e5c1c3029387d799
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-01 12:13:06 +0000

    Redirect After Form Submission

--- express_server.js
+++ express_server.js
@@ -41,10 +41,14 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  
-  res.send("Ok");         
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  //console.log(req.body);  // Log the POST request body to the console	  
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)	  
   res.redirect(`/urls/${shortURL}`);
-})
+ 
+        
+});	});
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
