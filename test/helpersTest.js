7663ef855adc8a40352f3f66ad2ab46fcc80c24f
amandaalliegro <amanda.m.arnaut@gmail.com>
2020-09-04 22:12:25 +0000

    done

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const { assert } = require('chai');
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", users)
+    const expectedOutput = "userRandomID";
+    assert.deepEqual(user, expectedOutput);
+  });
+  it('should return undefined when provided an invalid email', () => {
+    const user = getUserByEmail("test@example.com", testUsers);
+    const expectedOutput = undefined;
+    assert.deepEqual(user, expectedOutput);
+  });
+});
